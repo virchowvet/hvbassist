681f67c101436a435adb5c2fa17ad6a825d4860b
virchowvet <felipe.s.agostinho@gmail.com>
2025-05-17 08:23:17 -0300

    Update firestore.py

--- utils/firestore.py
+++ utils/firestore.py
@@ -3,14 +3,11 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Corrige os caracteres \\n e remove \r e \t residuais
+# Corrige \n duplos escapados para quebra de linha real
 raw_json = os.environ["FIREBASE_SERVICE_ACCOUNT_JSON"]
-cleaned_json = raw_json.replace("\\n", "\n").replace("\\r", "").replace("\\t", "")
-service_account_info = json.loads(cleaned_json)
+fixed_json = raw_json.replace('\\n', '\n')
+service_account_info = json.loads(fixed_json)
 
-# Inicializa o app do Firebase
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
-
-# Exporta o cliente do Firestore
 db = firestore.client()
