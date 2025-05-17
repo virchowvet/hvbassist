66fe79a9d3b33711cb38870c076604fb3184b397
virchowvet <felipe.s.agostinho@gmail.com>
2025-05-17 06:49:01 -0300

    Delete Hvbassist directory

--- Hvbassist/utils/firestore.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Substitua pelo caminho real da sua chave quando fizer o download
-cred = credentials.Certificate("utils/firebase_key.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
