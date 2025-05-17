2e1bbe06173af6767c6dc372be296924062e2768
virchowvet <felipe.s.agostinho@gmail.com>
2025-05-17 07:06:54 -0300

    Update firestore.py

--- utils/firestore.py
+++ utils/firestore.py
@@ -1,8 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Substitua pelo caminho real da sua chave quando fizer o download
-cred = credentials.Certificate("utils/firebase_key.json")
+# Lê a variável de ambiente que contém o JSON da chave de serviço
+service_account_info = json.loads(os.environ["FIREBASE_SERVICE_ACCOUNT_JSON"])
+
+# Inicializa o Firebase com as credenciais da variável
+cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
 
+# Cliente Firestore
 db = firestore.client()
